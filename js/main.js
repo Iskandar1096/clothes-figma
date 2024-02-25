48e7abc32dd4823d469332507422b68f2e3e18f0
Iskandar Ismailov <iit391109@gmail.com>
2024-02-25 22:06:31 +0300

    add carosel v.1

--- js/main.js
+++ js/main.js
@@ -54,3 +54,18 @@ for (let i = 0; i < liShopVal.length; i++) {
 
 
 
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+
+const itemWidth = 150
+const padding = 20
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= itemWidth + padding
+})
+
+next.addEventListener('click',()=>{
+  list.scrollLeft += itemWidth + padding
+})
+
